51d134add97bff28c542d10154e39a0ae4b7dd49
Dzanga <ratshikhopha.dr@gmail.com>
2026-04-24 01:18:52 +0200

    feat: Update loading screen and game scene
    - Modified the loading scene's camera background color and orthographic size for better visibility.
    - Adjusted the local position and scale of UI elements in the loading scene.
    - Added button functionality in MenuManager to handle game start and exit actions with debug logs.
    - Updated editor build settings to include the new game scene.
    - Created a new game scene with essential settings and components for gameplay.
    - Added necessary meta files for the new game scene and burst compilation settings.

--- Assets/Loading Screen/LoadingScreenCode/MenuManager.cs
+++ Assets/Loading Screen/LoadingScreenCode/MenuManager.cs
@@ -5,11 +5,13 @@ public class MenuManager : MonoBehaviour
 {
     public void PlayGame()
     {
+        Debug.Log("Play clicked");
         SceneManager.LoadScene("GameScene");
     }
 
     public void ExitGame()
     {
+        Debug.Log("Exit clicked");
         Application.Quit();
     }
 }
