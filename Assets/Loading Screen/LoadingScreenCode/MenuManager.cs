17a00ddf030aac32c53189efb9d01fd2208b7411
Dzanga <ratshikhopha.dr@gmail.com>
2026-04-23 23:30:35 +0200

    Refactor MenuManager class and implement PlayGame and ExitGame methods

--- Assets/Loading Screen/LoadingScreenCode/MenuManager.cs
+++ Assets/Loading Screen/LoadingScreenCode/MenuManager.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("GameScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Game closed");
     }
 }
